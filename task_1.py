03bbc6efb7714e51d01927b17fc753aa0b19ad6c
ad2896 <adhitya2896@gmail.com>
2022-03-07 20:20:04 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,10 @@
+from cmath import pi
+import math
+
+num = float(input( "Input the radius of the circle: "))
+area = pi *num*num
+
+print("The area of the circle with radius {} is: ".format(num))
+print(area)
+
+
