4c335138ed45055d6d15d296ce9497321e0e9846
ad2896 <adhitya2896@gmail.com>
2022-03-20 20:45:59 +0530

    Update str_frequency.py

--- str_frequency.py
+++ str_frequency.py
@@ -12,7 +12,6 @@ Output : i = 04 s = 04 p =02 m =01
 
 repeat = {}
 
-
 def most_frequent(str):
     for i in str:
         if i in repeat:
@@ -23,4 +22,12 @@ def most_frequent(str):
 
 most_frequent('Mississippi')
 
+
+key = [x for x, y in repeat.items()]
+value = [y for x, y in repeat.items()]
+
+for (a,b) in zip(key,value):
+    print(str(a)+"=0"+str(b))
+
+# NOTE: Im not aware how to sort the dictionary(descending order) .
 
