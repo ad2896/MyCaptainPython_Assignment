b71e91c5de4b127ef7ffad6310d4bffbd01f4e41
ad2896 <adhitya2896@gmail.com>
2022-03-21 00:05:36 +0530

    Update fibo.py

--- fibo.py
+++ fibo.py
@@ -1,23 +1,26 @@
 num = int(input("Enter a number to find fibonaci of first n numbers: "))
 
 def fibonaci(n):
-    a = 0
-    b = 1
-
     if num <= 0:
         print("Invlaid")
 
     elif num == 1:
+        a = 0
+        b = 1
         print(a)
+        
     else:
-        print(a)
-        print(b)
-    
-    for i in range(2,n):
-
-        c = a+b
-        a = b
-        b = c
-        print(c)
+        a = 0
+        b = 1
+        result = []
+        result += [a]
+        result += [b]
+        for i in range(2,n):
+            c = a+b
+            a = b
+            b = c
+            result += [c]
+        #print(c)
+        print("The list of first {} fibonaci series is :".format(num),result)
 
 fibonaci(num)
